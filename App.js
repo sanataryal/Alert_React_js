34330561f0b46468ccdbf7514e9b786880fa99e2
SANAT ARYAL <62319156+sanataryal@users.noreply.github.com>
2020-09-20 22:29:59 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './App.css';
+import Practice from './Components/Practice';
+
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+     
+      <Practice />
+       
+      </div>
+      
+    );
+  }
+}
+    
+export default App;
